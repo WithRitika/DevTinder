91675f3cc40a8e45275fafb80c654477236d7315
Ritika Soni <withritikasoni@gmnail.com>
2026-04-21 17:50:20 +0530

    Explore all kind of routes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,15 @@ app.listen(port,()=>{console.log(`App listen on port number:${port}`)});
 
 // This will only handle GET call to /user
 
+// http://localhost:7777/user?userId=101&name=Ritika&password=testing
 app.get("/user", (req,res)=>{
+    console.log(req.query);
+    res.send({firstName:"Ritika",lastName:"Soni"});
+});
+
+// Dynamic routes  http://localhost:7777/user/102/Ritika/test
+app.get("/user/:userId/:name/:password", (req,res)=>{
+    console.log(req.params);
     res.send({firstName:"Ritika",lastName:"Soni"});
 });
 
@@ -21,6 +29,7 @@ app.delete("/user",(req,res)=>{
 });
 
 // This will match all the HTTP method api calls to test
-app.use('/user',(req,res)=>{
+ app.get(/\/ab*c/,(req,res)=>{
     res.send('Hello from the test server');
 });
+
