870d6b33afa505545c810334a4a23ef05908cf83
Ritika Soni <withritikasoni@gmnail.com>
2026-04-21 17:21:00 +0530

    Exploring get post delete

--- src/app.js
+++ src/app.js
@@ -4,14 +4,23 @@ const port=7777
  
 app.listen(port,()=>{console.log(`App listen on port number:${port}`)});
 
-app.use('/test',(req,res)=>{
-    res.send('Hello from the server');
+
+// This will only handle GET call to /user
+
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Ritika",lastName:"Soni"});
 });
 
-app.use('/hello',(req,res)=>{
-    res.send('Hello Hello Hello');
+app.post("/user",(req,res)=>{
+    // Saving data to db
+    res.send("Data successfully saved to the database");
 });
 
-app.use('/',(req,res)=>{
-    res.send('Hello from the Ritika Soni');
+app.delete("/user",(req,res)=>{
+    res.send("User delete successfully");
+});
+
+// This will match all the HTTP method api calls to test
+app.use('/user',(req,res)=>{
+    res.send('Hello from the test server');
 });
